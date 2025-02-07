50c9941a0619e8f9643e286d71d15760e46942f4
Miguel-Rios1094 <164534109+Miguel-Rios1094@users.noreply.github.com>
2025-02-07 16:47:44 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,19 @@
+//funcion que no devuelve valor
+
+void saludar() {
+  print('Hola mundo');
+}
+
+void escribeNombre() {
+  print('Miguel Montelongo');
+}
+
+void main() {
+  // Miguel Montelongo mat:22308051281094
+  // llamar a la funcion saludar
+  print('Miguel Montelongo mat:22308051281094');
+  print('llamar a la funcion saludar');
+  saludar();
+  print('llamar a la funcion escribeNombre');
+  escribeNombre();
+}
