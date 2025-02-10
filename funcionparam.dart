906de0eecfadf3ee2879a59722eb885a95195772
Miguel-Rios1094 <164534109+Miguel-Rios1094@users.noreply.github.com>
2025-02-10 18:50:29 +0000

    Funcion parametros

--- funcionparam.dart
+++ funcionparam.dart
@@ -1,7 +1,8 @@
+//funcion saludar
 double rectangulo (double ancho, double alto) {
   return ancho * alto;
 }
-
+//funcion saludar
 void saludar(String nombre) {
   print('Hola, $nombre! Bienvenido');
 }
@@ -9,8 +10,10 @@ void saludar(String nombre) {
 void main() {
   print('Miguel Eduardo Rios Montelongo: 22308051281094');
   print('llamar a la funcion area');
+  //llamada a la funcion rectangulo
   double area = rectangulo(10, 5);
   print("el area del rectangulo es: $area");
   print('llamar a la funcion saludar');
+  //llamada a la funcion saludar
   saludar('Miguel');
 }
