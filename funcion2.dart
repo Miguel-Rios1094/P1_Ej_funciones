a7a03882d15630b2a4bcd3a48f1f3070e92f42c6
Miguel-Rios1094 <164534109+Miguel-Rios1094@users.noreply.github.com>
2025-02-10 18:04:23 +0000

    ejemplo funcion rama

---
+++ funcion2.dart
@@ -0,0 +1,16 @@
+//funciones creadas por el usuario
+
+void suma (int a, int b) {
+  print('la Suma es: ${a + b}');
+}
+
+void resta (int a, int b) {
+  print('la Resta es: ${a - b}');
+}
+//funcion main
+void main() {
+  print('Miguel Eduardo Rios Montelongo: 22308051281094');
+  print('llamar a la funcion suma y resta');
+  suma(10, 5);
+  resta(10, 5);
+}
