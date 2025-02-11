7ca36a4164c301f95a660a88f00f660fea25f86f
Miguel-Rios1094 <164534109+Miguel-Rios1094@users.noreply.github.com>
2025-02-11 20:29:21 +0000

    Actualizacon lista

--- problema5.dart
+++ problema5.dart
@@ -1,11 +1,33 @@
+import 'dart:io';
+
 void main() {
-  // Miguel Montelongo mat:22308051281094
+  // Función para capturar los datos de la lista
+  List<String> capturarLista() {
+    List<String> lista = [];
+    print("Ingrese el número de cadenas que desea agregar a la lista:");
+    int cantidad = int.parse(stdin.readLineSync()!);
 
-  print('Miguel Montelongo mat:22308051281094');
+    for (int i = 0; i < cantidad; i++) {
+      print("Ingrese la cadena ${i + 1}:");
+      String cadena = stdin.readLineSync()!;
+      lista.add(cadena);
+    }
 
-  List<String> cadenas = ["miguel eduardo rios montelongo", "numero de control","22308051281094", "6i", "programación"];
-  
-  List<String> Mayusculas = cadenas.map((cadena) => cadena.toUpperCase()).toList();
-// imprimir lista en mayusculas
-  print(Mayusculas);
+    return lista;
+  }
+
+  // Función para mostrar los elementos de la lista en mayúsculas
+  void mostrarListaEnMayusculas(List<String> lista) {
+    List<String> listaMayusculas = lista.map((cadena) => cadena.toUpperCase()).toList();
+    print("\nLista en mayúsculas:");
+    for (String cadena in listaMayusculas) {
+      print(cadena);
+    }
+  }
+
+  // Capturar la lista de cadenas
+  List<String> listaOriginal = capturarLista();
+
+  // Mostrar la lista en mayúsculas
+  mostrarListaEnMayusculas(listaOriginal);
 }
