54f889196e00f3e366ce346a1c5dd108574d15ee
Miguel-Rios1094 <164534109+Miguel-Rios1094@users.noreply.github.com>
2025-02-11 20:24:00 +0000

    actualizar lista

---
+++ problema5.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Miguel Montelongo mat:22308051281094
+
+  print('Miguel Montelongo mat:22308051281094');
+
+  List<String> cadenas = ["miguel eduardo rios montelongo", "numero de control","22308051281094", "6i", "programación"];
+  
+  List<String> Mayusculas = cadenas.map((cadena) => cadena.toUpperCase()).toList();
+// imprimir lista en mayusculas
+  print(Mayusculas);
+}
---
+++ recurso/problema5.md
@@ -0,0 +1,2 @@
+5.-Crea un programa que tome una lista de cadenas (string) y devuelva una nueva lista con todas las cadenas convertidas a mayúsculas.
+![alt text](image-5.png)
